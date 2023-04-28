214a78f4e5fd930be63eecabc8ec6937f6cf1cd5
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-04-28 12:00:20 -0300

    Update neotree config

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -4,6 +4,17 @@ return {
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
+        -- hide_gitignored = true,
+        hide_by_name = {
+          --"node_modules"
+        },
+        hide_by_pattern = { -- uses glob style patterns
+          --"*.meta",
+          --"*/src/*/tsconfig.json",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          ".gitignored",
+        },
       },
     },
   },
