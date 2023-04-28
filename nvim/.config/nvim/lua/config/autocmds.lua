3567eaf161f793a33b006ded1f3c629e58811c34
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-04-28 10:11:02 -0300

    Add <C-w> keybind in vim

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- add whitespace at the end of file after save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  -- command = ":%s/($\ns*)+%$/\r/",
+  -- callback = function()
+  --   vim.cmd(":%s/($\ns*)+%$/AAAAAAA\r/<CR>")
+  -- end,
+})
