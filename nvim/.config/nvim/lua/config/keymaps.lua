d197a8258a0aa0bb1a6c5750041ea795ee982bc7
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2023-05-17 17:58:58 +0000

    Update vim keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,19 +4,26 @@ local m = require("mapx").setup({ global = "skip", whichkey = true })
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- use ; as : to run commands
+m.nmap(";", ":")
+m.vmap(";", ":")
+
 -- stuff imported from vscode
-m.nmap("n", "<C-w>", ":clo<CR>")
+-- m.map("<C-w>", ":clo<CR>", "Close window")
 
 -- toggle file tree
-m.nmap("<Leader>pv", ":Neotree toggle<CR>")
+m.nmap("<Leader>pv", ":Neotree toggle<CR>", "Toggle file tree")
 
 -- Toggle comments (this is equivalent to gcc)
--- map("n", "<C-S-a>", ":Commentary<CR>")
-m.nmap("<Leader>q", ":Commentary<CR>")
-m.vmap("<Leader>q", ":Commentary<CR>")
+m.nmap("<Leader>q", ":Commentary<CR>", "Toggle comment")
+m.vmap("<Leader>q", ":Commentary<CR>", "Toggle comment")
+m.inoremap("<C-a>", "<cmd>Commentary<CR>", "Toggle comment")
 
 -- Add empty lines
 m.nnoremap("<Leader>k", ":set paste<CR>m`O<Esc>``:set nopaste<CR>", "Insert empty line above")
 m.nnoremap("<Leader>j", ":set paste<CR>m`o<Esc>``:set nopaste<CR>", "Insert empty line below")
 
 m.nmap("<Leader>td", "<Leader>kkgccA TODO: ", "Add TODO comment")
+
+-- Clear search
+m.map("<Leader>sq", ':let @/ = ""<CR>', "Clear search")
